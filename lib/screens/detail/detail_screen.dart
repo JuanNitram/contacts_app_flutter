ba9986fccc392fd3de52f714065115340356a217
Juan <juanmvg2006@gmail.com>
2020-12-31 15:43:09 -0300

    Add details screen

--- lib/screens/detail/detail_screen.dart
+++ lib/screens/detail/detail_screen.dart
@@ -13,13 +13,105 @@ class DetailScreen extends StatelessWidget {
   Widget build(BuildContext context) {
     // Use the Todo to create the UI.
     return Scaffold(
-      appBar: AppBar(
-        title: Text(contact.name),
-      ),
-      body: Padding(
-        padding: EdgeInsets.all(16.0),
-        child: Text(contact.address),
-      ),
-    );
+        appBar: AppBar(
+          iconTheme: IconThemeData(
+            color: Colors.deepPurple[500], //change your color here
+          ),
+          backgroundColor: Colors.white,
+          toolbarHeight: 70,
+        ),
+        body: Container(
+          padding: EdgeInsets.all(20.0),
+          child: Column(children: <Widget>[
+            Row(children: <Widget>[
+              CircleAvatar(
+                backgroundImage: NetworkImage(contact.avatar),
+                radius: 38,
+              ),
+              Container(
+                height: 100,
+                padding: EdgeInsets.fromLTRB(20, 20, 20, 0),
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: <Widget>[
+                    Text(contact.name,
+                        style: TextStyle(
+                            fontSize: 30,
+                            fontWeight: FontWeight.bold,
+                            color: Colors.black)),
+                    Flexible(
+                      child: Text(contact.about,
+                          style: TextStyle(
+                              fontSize: 16, color: Colors.deepPurple[500])),
+                    )
+                  ],
+                ),
+              )
+            ]),
+            Padding(padding: EdgeInsets.fromLTRB(0, 10, 0, 10)),
+            Row(children: <Widget>[
+              Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: <Widget>[
+                    Text(
+                      'Address',
+                      style: TextStyle(
+                          fontSize: 24,
+                          color: Colors.black,
+                          fontWeight: FontWeight.bold),
+                    ),
+                    Text(
+                      contact.address,
+                      style: TextStyle(
+                        fontSize: 16,
+                        color: Colors.deepPurple[500],
+                      ),
+                    )
+                  ])
+            ]),
+            Padding(padding: EdgeInsets.fromLTRB(0, 10, 0, 10)),
+            Row(children: <Widget>[
+              Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: <Widget>[
+                    Text(
+                      'Phone',
+                      style: TextStyle(
+                          fontSize: 24,
+                          color: Colors.black,
+                          fontWeight: FontWeight.bold),
+                    ),
+                    Text(
+                      contact.phone,
+                      style: TextStyle(
+                        fontSize: 16,
+                        color: Colors.deepPurple[500],
+                      ),
+                    )
+                  ])
+            ]),
+            Padding(padding: EdgeInsets.fromLTRB(0, 10, 0, 10)),
+            Row(children: <Widget>[
+              Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: <Widget>[
+                    Text(
+                      'Email',
+                      style: TextStyle(
+                          fontSize: 24,
+                          color: Colors.black,
+                          fontWeight: FontWeight.bold),
+                    ),
+                    Text(
+                      'email@email.com',
+                      style: TextStyle(
+                        fontSize: 16,
+                        color: Colors.deepPurple[500],
+                      ),
+                    )
+                  ])
+            ])
+          ]),
+        ));
   }
 }
